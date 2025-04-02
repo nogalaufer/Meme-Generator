69331f5de5d9baa56d63e2fe31ba8ea24cbe7624
Noga Laufer <nogalaufer1@gmail.com>
2025-04-02 13:39:06 +0300

    make onSearch() to get TXT only, not with filter

--- main/controller.js
+++ main/controller.js
@@ -1,25 +1,35 @@
 'use strict'
 
+// search
+$('#searchWord').change('keydown',onSearch )
+$('#searchBtn').click('input', onSearch)
 
-var gKeyWordSearchCountMap = {'funny': 12,'cat': 16, 'baby': 2} 
+var gKeyWordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
 
 
- 
 
-    function onInit() {
-        console.log('init')
-        renderGallery()
-    
-    }
 
-function renderGallery(){
+function onInit() {
+    console.log('init')
+    renderGallery()
+
+}
+
+function onSearch(ev) {
+    var searchWord = $('#searchWord').val().toLowerCase()
+    console.log(searchWord)
+
+}
+
+
+function renderGallery() {
     var strHTMLs = ''
     if (gImgs && gImgs.length > 0) {
-        strHTMLs= gImgs.map(img => {
+        strHTMLs = gImgs.map(img => {
             return `<img src ="${img.url}" alt="${img.id}">`
         }).join('')
     }
-$('.gallery-container').html(strHTMLs)
-  
+    $('.gallery-container').html(strHTMLs)
+
 
 }
